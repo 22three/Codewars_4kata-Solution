acc83aef6720b5d613961f08f08f80e8d50c62e9
22three <eremeevv513@gmail.com>
2024-03-14 23:09:12 +0300

    Добавьте файлы проекта.

---
+++ CodewarsC#/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Codewars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            int expectedrank = -8;
+            int appliedrank = -7;
+            user.rank = expectedrank;
+            user.progress = 0;           
+            user.incProgress(appliedrank);
+            Console.WriteLine($"Applied Rank: {appliedrank} \nExpected Rank: {expectedrank} \nActual: {user.rank} \nProgress: {user.progress}");
+        }
+    }
+
+    public class User
+    {
+        public int rank;
+        public int progress;
+        public int[] incProgress(int actRank)
+        {
+            if (actRank > 8 || actRank < -8 || actRank == 0)
+                throw new ArgumentException();
+            int _rank = actRank - rank;   
+            if (actRank >= 1)
+                _rank--;
+            if (_rank == 0)
+                progress += 3;
+            else
+            progress = 10 * _rank * _rank;
+            if (progress >= 100)
+                rankUP();
+            return [rank,progress];
+        }
+        public void rankUP()
+        {
+            if (rank<8)
+            while (progress >= 100)
+            {
+                rank++;
+                progress -= 100;
+            }
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,46 @@
+namespace TestProject1
+{
+    using Codewars;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class Tests
+    {
+        public void assertRankProgression(int rank, User user, int expRank, int expProgress)
+        {
+            Assert.True(user.rank == expRank,
+              "Applied Rank: " + rank +
+              "; Expected rank: " + expRank +
+              "; Actual: " + user.rank);
+
+            Assert.True(user.progress == expProgress,
+              "Applied Rank; " + rank +
+              "; Expected progress: " + expProgress +
+              ", Actual: " + user.progress);
+        }
+
+        [TestCase(-7, -8, 10)]
+        [TestCase(-6, -8, 40)]
+        [TestCase(-5, -8, 90)]
+        [TestCase(-4, -7, 60)]
+        [TestCase(-8, -8, 3)]
+        // Check single increments
+        public void testValidSingleRankProgression(int rank, int expectedRank, int expectedProgress)
+        {
+            User user = new User();
+            user.incProgress(rank);
+            assertRankProgression(rank, user, expectedRank, expectedProgress);
+        }
+
+        [TestCase(9)]
+        [TestCase(-9)]
+        [TestCase(0)]
+        // Check invalid rank progressions
+        public void testInvalidRange(int rank)
+        {
+            User user = new User();
+            Assert.Throws<ArgumentException>(() => user.incProgress(rank));
+        }
+    }
+}
